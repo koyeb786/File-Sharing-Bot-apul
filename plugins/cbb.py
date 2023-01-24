3dabc4d62373c207e8ad969d60f10cb7d82e9957
koyeb786 <121357533+koyeb786@users.noreply.github.com>
2023-01-24 09:23:06 -0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>★彡 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href='tg://user?id={OWNER_ID}'>【💚➣𝙰𝙼𝙸𝚃➣💚】</a>\n★彡 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁 : <code>𝙷𝙴𝚁𝙾𝙺𝚄</code>\n★彡 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 : <a href='https://www.youtube.com'>💕 </a>\n★彡 BACK UP : <a href='https://t.me/+b6ZO-tR70bkzNDM1'>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>\n★彡 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱\n★彡 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 : @apul_group_neeting_meeting</b>",
+            text = f"<b>★彡 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href='tg://user?id={OWNER_ID}'>【💚➣𝙰𝙼𝙸𝚃➣💚】</a>\n★彡 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁 : <code>𝙷𝙴𝚁𝙾𝙺𝚄</code>\n★彡 BACK UP : <a href='https://t.me/+b6ZO-tR70bkzNDM1'>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>\n★彡 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱\n★彡 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 : @apul_group_neeting_meeting</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
